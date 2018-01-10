2c032753baf07ff05c4556600a8e8ed0f93250e2
ljw <shen1577@126.com>
2018-01-10 19:30:48 +0800

    lxml

--- python_spider/crawling.py
+++ python_spider/crawling.py
@@ -8,6 +8,14 @@ Created on Tue Jan 09 12:48:40 2018
 import re 
 from bs4 import BeautifulSoup
 
+FIELDS = ('area','population','iso','country','capital','continent',
+          'tld','currency_code','currency_name','phone','postal_code_format',
+          'postal_code_regex','languages','neighbbours')
+
+def re_scraper(html):
+    results={}
+    for field in FIELDS:
+        results[field]=re.search('<tr id="places_%s__row">')
 
 broken_html='<ul class=country><li>Area<li>Population</ul>'
 soup=BeautifulSoup(broken_html,'html.parser')
@@ -18,3 +26,8 @@ ul=soup.find('ul',attrs={'class':'country'})
 ul.find('li')
 
 
+import lxml.html
+broken_url='<ul class=country><li>Area<li>Population</ul>'
+tree=lxml.html.fromstring(broken_url)
+fixed_url=lxml.html.tostring(tree,pretty_print=True)
+print fixed_url
